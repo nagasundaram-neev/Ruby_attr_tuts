655c9622248f5dcbc510df18b48667c7d02275da
nagasundaram-neev <nagasundaram@neevtech.com>
2013-09-03 13:53:19 +0530

    Commit with attr_writer & attr_reader methods

--- car.rb
+++ car.rb
@@ -1,14 +1,11 @@
 class Car
-  def brand=(brand)  #Setter method. Shorthand way
-    @brand=brand
-  end
-  def brand  #getter method Shorthand way
-    @brand
-  end
+  #Setter method. using attr_writer
+  attr_writer :brand 
+  #getter method using attr_reader
+  attr_reader :brand
 end
 
  
 c1=Car.new
 c1.brand="audi" # use the setter method
 puts c1.brand  #use the getter method
-puts c1.inspect
